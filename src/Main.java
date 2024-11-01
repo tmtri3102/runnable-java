a0e0a8369bc951f89a9d5ba6afcfe9e65f49a59c
tmtri3102 <tmtri310251@gmail.com>
2024-11-01 13:43:32 +0700

    Practice Runnable

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main thread running... ");
+
+        RunnableDemo runnableDemo1 = new RunnableDemo("Thread-1-HR-Database"); // Creating Thread-1-HR-Database after "new" construction
+        runnableDemo1.start(); // run() ngay sau khi start()??
+
+        RunnableDemo runnableDemo2 = new RunnableDemo("Thread-2-Send-Email");
+        runnableDemo2.start();
+
+        System.out.println("Main thread stopped!!! ");
+    }
+}
